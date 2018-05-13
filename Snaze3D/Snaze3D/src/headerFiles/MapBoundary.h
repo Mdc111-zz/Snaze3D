b2c28bb49b4f58a746d28e5962fc0a1251f8dde0
Unknown <matt_cooper1@hotmail.com>
2018-05-13 23:12:08 +0100

    Added a map boundary model class

---
+++ Snaze3D/Snaze3D/src/headerFiles/MapBoundary.h
@@ -0,0 +1,12 @@
+#ifndef BoundaryHeader
+#define BoundaryHeader
+
+class MapBoundary {
+public:
+	int leftBound = -15;
+	int rightBound = 15;
+	int upBound = 15;
+	int botBound = -15;
+};
+
+#endif // !BoundaryHeader
