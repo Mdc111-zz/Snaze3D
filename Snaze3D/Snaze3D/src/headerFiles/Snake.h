2d731ddb555b4a95f553c3147abce3c0111df1aa
Unknown <matt_cooper1@hotmail.com>
2018-05-13 23:13:53 +0100

    Added additional variables to snake header file and new function declaration

--- Snaze3D/Snaze3D/src/headerFiles/Snake.h
+++ Snaze3D/Snaze3D/src/headerFiles/Snake.h
@@ -2,12 +2,17 @@
 #define SNAKEHEADER
 
 #include <glut.h>
+
 class Snake {
 public:
 	GLint   snakeBodyPos[2][100] = { {} };
 	GLint   snakeXPos = 5;
 	GLint   snakeYPos = 10;
+	GLint   lastSnakeXPos[2] = {};
+	GLint   lastSnakeYPos[2] = {};
+	GLbyte  direction = 0;
 	GLint   snakeSize = 0;
 	void DrawSnake();
+	void SaveSnakePosition();
 };
 #endif
